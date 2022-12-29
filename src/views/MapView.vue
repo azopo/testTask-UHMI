82fbce065b27d8b86937fe65966024aa76bace56
azopo <alexhorb@icloud.com>
2022-12-29 11:54:14 +0200

    add map page;

---
+++ src/views/MapView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="map" class="h-5 w-5"></div>
+</template>
+
+<script>
+import L from "leaflet";
+
+export default {
+  name: "MapView",
+
+  mounted() {
+    // створюємо нову карту із вибраними координатами та збільшуємо масштаб
+    this.map = L.map("map").setView([51.505, -0.09], 13);
+
+    // додаємо тайли карти з OpenStreetMap
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18,
+    }).addTo(this.map);
+  },
+};
+</script>
+
+<style scoped></style>
