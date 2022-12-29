6af09d572460d7a1fd5bf664aef62607bcaf77c5
azopo <alexhorb@icloud.com>
2022-12-29 14:12:42 +0200

    add map page;

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
+  <div class="container mb-8">
+    <router-link
+      class="mr-7 text-5xl hover:text-black transition"
+      active-class="border-b"
+      to="/"
+      >Home</router-link
+    >
+    <router-link
+      class="text-5xl hover:text-black transition"
+      to="/map"
+      active-class="border-b"
+      >Map</router-link
+    >
+  </div>
   <RouterView />
 </template>
+<style scoped lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,49 @@
 <template>
-  <div id="map" class="h-5 w-5"></div>
+  <div class="container mx-auto h-screen">
+    <div ref="map" class="w-full h-full z-40" @click="openModal" />
+  </div>
+  <div
+    :class="modal ? 'block' : 'hidden'"
+    class="fixed top-0 left-0 right-0 bottom-0 z-50 overflow-y-auto"
+  >
+    <div class="container mx-auto px-4 py-4">
+      <div class="bg-white rounded shadow p-4">
+        <h2 class="text-xl font-bold mb-2 text-black">Додати маркер</h2>
+        <div>
+          <input
+            v-model="nameMarker"
+            class="w-full border rounded px-2 py-1 text-black mb-1"
+            type="text"
+            placeholder="Назва маркера"
+          />
+          <input
+            v-model="lat"
+            class="w-full border rounded px-2 py-1 text-black mb-1"
+            type="text"
+            placeholder="Широта"
+          />
+          <input
+            v-model="lng"
+            class="w-full border rounded px-2 py-1 text-black mb-1"
+            type="text"
+            placeholder="Довгота"
+          />
+        </div>
+        <button
+          @click="addMarker"
+          class="bg-green-700 text-white rounded px-4 py-2"
+        >
+          Додати
+        </button>
+        <button
+          @click="closeModal"
+          class="bg-red-500 text-white rounded px-4 py-2"
+        >
+          Закрити
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,19 +51,52 @@ import L from "leaflet";
 
 export default {
   name: "MapView",
-
+  data() {
+    return {
+      map: null,
+      modal: false,
+      lat: null,
+      lng: null,
+      nameMarker: null,
+    };
+  },
   mounted() {
-    // створюємо нову карту із вибраними координатами та збільшуємо масштаб
-    this.map = L.map("map").setView([51.505, -0.09], 13);
-
-    // додаємо тайли карти з OpenStreetMap
+    this.map = L.map(this.$refs.map).setView([50.4504, 30.5234], 6);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
     }).addTo(this.map);
   },
+  methods: {
+    openModal(event) {
+      this.modal = true;
+      const { lat, lng } = this.map.mouseEventToLatLng(event);
+      this.lng = lng;
+      this.lat = lat;
+    },
+    closeModal() {
+      this.lng = null;
+      this.lat = null;
+      this.nameMarker = null;
+      this.modal = false;
+    },
+    addMarker() {
+      const marker = L.marker([this.lat, this.lng]).addTo(this.map);
+      marker.bindTooltip(this.nameMarker);
+      this.lng = null;
+      this.lat = null;
+      this.nameMarker = null;
+      this.modal = false;
+    },
+  },
+  beforeUnmount() {
+    this.map.remove();
+  },
 };
 </script>
-
-<style scoped></style>
+<style lang="scss" scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
